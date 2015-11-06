5909640f5285e8b30354b106088eb28e35800b6a
mecctro <mecctro@users.noreply.github.com>
2015-11-06 15:13:04 -0500

    Update stack-install.sh

--- stack-install.sh
+++ stack-install.sh
@@ -6,6 +6,7 @@ cd tengine &&
 mv packages/debian . &&
 DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -rfakeroot -uc -b &&
 dpkg -i ../tengine_*.deb &&
+rm -rf /etc/init.d/tengine &&
 cp debian/init.d /etc/init.d/nginx &&
 chmod 775 /etc/init.d/nginx &&
 update-rc.d nginx defaults &&
@@ -24,12 +25,15 @@ make install &&
 cd /usr/local/mysql &&
 chown -R mysql . &&
 chgrp -R mysql . &&
-/usr/local/mysql/scripts/mysql_install_db -defaults-file=/usr/local/mysql/my.cnf --user=mysql &&
+sed -i 's|datadir.*|datadir         = /usr/local/mysql/data|g' /etc/mysql/my.cnf &&
+sed -i 's|lc-messages-dir.*|lc-messages-dir = /usr/local/mysql/share|g' /etc/mysql/my.cnf &&
+sudo /usr/local/mysql/scripts/mysql_install_db -defaults-file=/usr/local/mysql/my.cnf --user=mysql &&
 mv support-files/mysql.server /etc/init.d/mysql &&
 chmod 775 /etc/init.d/mysql &&
 update-rc.d mysql defaults &&
 service mysql restart &&
 cd ../ &&
+ln -s /usr/local/mysql/bin/* /usr/bin &&
 rm -rf webscalsql* ;
 
 apt-get install gfortran -y ;
@@ -46,5 +50,12 @@ rm -rf fcgi* ;
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449 &&
 echo deb http://dl.hhvm.com/debian wheezy main | sudo tee /etc/apt/sources.list.d/hhvm.list &&
 apt-get update &&
-apt-get install hhvm -y
+apt-get install hhvm -y &&
+update-rc.d hhvm defaults &&
+/usr/share/hhvm/install_fastcgi.sh &&
+sed -i 's/9000/8000/g' /etc/hhvm/server.ini &&
+sed -i 's/9000/8000/g' /etc/nginx/hhvm.conf &&
+echo 'fastcgi_keep_conn on;' >> /etc/nginx/fastcgi.conf &&
+
+apt-get install php5-fpm -y &&
 
