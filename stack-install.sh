9f9c87c0e5f2abe57cd0369f3f20f82b62bc387d
mecctro <mecctro@users.noreply.github.com>
2015-11-06 12:20:04 -0500

    Update stack-install.sh

--- stack-install.sh
+++ stack-install.sh
@@ -5,6 +5,9 @@ cd tengine
 mv packages/debian .
 DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -rfakeroot -uc -b
 dpkg tengine_*
+cp packages/debian/init.d /etc/init.d/nginx
+update-rc.d nginx defaults
+service nginx restart
 rm -rf tengine*
 
 apt-get install cmake gcc libaio-dev libncurses5-dev libreadline-dev bison perl -y
@@ -19,6 +22,9 @@ cd /usr/local/mysql
 chown -R mysql .
 chgrp -R mysql .
 /usr/local/mysql/scripts/mysql_install_db -defaults-file=/usr/local/mysql/my.cnf --user=mysql
+mv support-files/mysql.server /etc/init.d/mysql
+chmod 775 /etc/init.d/mysql
+update-rc.d mysql defaults
 
 apt-get install gfortran
 
@@ -28,4 +34,9 @@ cd fcgi*
 ./configure
 make
 make install
+
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
+echo deb http://dl.hhvm.com/debian wheezy main | sudo tee /etc/apt/sources.list.d/hhvm.list
+apt-get update
+apt-get install hhvm -y
 
