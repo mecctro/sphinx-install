b26dfc26f0d2ead2c8805c6f523262c1159e7836
mecctro <mecctro@users.noreply.github.com>
2015-11-06 12:52:22 -0500

    Update stack-install.sh

--- stack-install.sh
+++ stack-install.sh
@@ -1,49 +1,49 @@
 apt-get install git-core -y
 apt-get install build-essential debhelper make autoconf automake patch \
  dpkg-dev fakeroot pbuilder gnupg dh-make libssl-dev libpcre3-dev -y
-git clone https://github.com/alibaba/tengine.git
-cd tengine
-mv packages/debian .
-DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -rfakeroot -uc -b
-dpkg -i tengine_*
-cp packages/debian/init.d /etc/init.d/nginx
-update-rc.d nginx defaults
-service nginx restart
-cd ../
-rm -rf tengine*
+git clone https://github.com/alibaba/tengine.git &&
+cd tengine &&
+mv packages/debian . &&
+DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -rfakeroot -uc -b &&
+dpkg -i ../tengine_*.deb &&
+cp packages/debian/init.d /etc/init.d/nginx &&
+update-rc.d nginx defaults &&
+service nginx restart &&
+cd ../ &&
+rm -rf tengine* ;
 
-apt-get install cmake gcc libaio-dev libncurses5-dev libreadline-dev bison perl -y
-git clone https://github.com/webscalesql/webscalesql-5.6.git
-cd webscalesql*
-groupadd mysql
-useradd -r -g mysql mysql
-cmake .
-make
-make install
-cd /usr/local/mysql
-chown -R mysql .
-chgrp -R mysql .
-/usr/local/mysql/scripts/mysql_install_db -defaults-file=/usr/local/mysql/my.cnf --user=mysql
-mv support-files/mysql.server /etc/init.d/mysql
-chmod 775 /etc/init.d/mysql
-update-rc.d mysql defaults
-service mysql restart
-cd ../
-rm -rf webscalsql*
+apt-get install cmake gcc libaio-dev libncurses5-dev libreadline-dev bison perl -y &&
+git clone https://github.com/webscalesql/webscalesql-5.6.git &&
+cd webscalesql* &&
+groupadd mysql &&
+useradd -r -g mysql mysql &&
+cmake . &&
+make &&
+make install &&
+cd /usr/local/mysql &&
+chown -R mysql . &&
+chgrp -R mysql . &&
+/usr/local/mysql/scripts/mysql_install_db -defaults-file=/usr/local/mysql/my.cnf --user=mysql &&
+mv support-files/mysql.server /etc/init.d/mysql &&
+chmod 775 /etc/init.d/mysql &&
+update-rc.d mysql defaults &&
+service mysql restart &&
+cd ../ &&
+rm -rf webscalsql* ;
 
-apt-get install gfortran
+apt-get install gfortran -y ;
 
-wget http://www.fastcgi.com/dist/fcgi.tar.gz
-tar -xzf fcgi.tar.gz
-cd fcgi*
-./configure
-make
-make install
-cd ../
-rm -rf fcgi*
+wget http://www.fastcgi.com/dist/fcgi.tar.gz &&
+tar -xzf fcgi.tar.gz &&
+cd fcgi* &&
+./configure &&
+make &&
+make install &&
+cd ../ &&
+rm -rf fcgi* ;
 
-apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
-echo deb http://dl.hhvm.com/debian wheezy main | sudo tee /etc/apt/sources.list.d/hhvm.list
-apt-get update
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449 &&
+echo deb http://dl.hhvm.com/debian wheezy main | sudo tee /etc/apt/sources.list.d/hhvm.list &&
+apt-get update &&
 apt-get install hhvm -y
 
