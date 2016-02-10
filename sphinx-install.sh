3ca08c7a96d8e339d8c23f2490811df3cc2588c2
mecctro <mecctro@users.noreply.github.com>
2016-02-10 07:26:46 -0500

    Update sphinx-install.sh

--- sphinx-install.sh
+++ sphinx-install.sh
@@ -2,7 +2,6 @@ apt-get install git-core -y
 apt-get install build-essential debhelper make autoconf automake patch \
  dpkg-dev fakeroot pbuilder gnupg dh-make libssl-dev libpcre3-dev libmysqlclient-dev -y &&
 git clone https://github.com/sphinxsearch/sphinx.git &&
-cd sphinx
-./configure
-make -j 2
-make install
+cd sphinx &&
+./configure --prefix=/usr/local/sphinx &&
+make -j 4 install
